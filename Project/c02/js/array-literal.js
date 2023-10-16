a415dde389944195482bdcdd2bf7d930df1e9755
anhtrinh122 <127742458+anhtrinh122@users.noreply.github.com>
2023-10-16 09:57:05 +0700

    .

---
+++ Project/c02/js/array-literal.js
@@ -0,0 +1,5 @@
+var colors;
+colors = ['white', 'black', 'custom'];
+
+var el = document.getElementById('color');
+el.textContent = colors[0];
---
+++ Project/c02/js/string-operator.js
@@ -0,0 +1,9 @@
+var greeting = 'Howdy';
+
+var name = 'Molly';
+
+var welcomeMessage = greeting + name + '!';
+
+var el = document.getElementById('gretting');
+
+el.textContent = welcomeMessage;
